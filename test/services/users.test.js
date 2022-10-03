eb72fa8ea329d5f5070aa332384911192a424406
Barno <32536886+CarolBarno@users.noreply.github.com>
2022-10-03 22:53:16 +0300

    services test set up (#6)
    Co-authored-by: Carol-Barno <cbarno@innovexsolutions.co.ke>

--- test/services/users.test.js
+++ test/services/users.test.js
@@ -7,4 +7,24 @@ describe('\'users\' service', () => {
 
     assert.ok(service, 'Registered the service');
   });
+
+  it('creates a user and encrypts password', async () => {
+    const user = await app.service('users').create({
+      email: 'test@example.com',
+      password: 'secret'
+    });
+
+    assert.ok(user.password !== 'secret');
+  });
+
+  it('removes password for external requests', async () => {
+    const params = { provider: 'rest' };
+
+    const user = await app.service('users').create({
+      email: 'test@example.com',
+      password: 'secret'
+    }, params);
+
+    assert.ok(!user.password);
+  });
 });
