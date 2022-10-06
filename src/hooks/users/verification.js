f4b0438e463de3cbe44c8d9dcaa79c8ac5c2ad1b
Barno <32536886+CarolBarno@users.noreply.github.com>
2022-10-06 21:05:40 +0300

    auth access token + email verification (#8)
    Co-authored-by: Carol-Barno <cbarno@innovexsolutions.co.ke>

--- src/hooks/users/verification.js
+++ src/hooks/users/verification.js
@@ -6,32 +6,32 @@ const { GeneralError } = require('@feathersjs/errors');
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
   return async context => {
-    const {app, params, data} = context;
+    const { app, params, data } = context;
     delete data.action;
     let objKey = Object.keys(data).filter(o => o == 'email' || o == 'phone_number');
     let key = objKey.join();
     context.validation = true;
 
-    switch(data[key]) {
-    case data.email: {      
+    switch (data[key]) {
+    case data.email: {
       const email = data.email;
-      let found = await app.service('users')._find({query: {email}}, params);
+      let found = await app.service('users')._find({ query: { email } }, params);
 
-      if(found.total) {
-        throw new GeneralError('Email is already registered', {taken: true});
-      }else {
-        context.result = {taken: false};
+      if (found.total) {
+        throw new GeneralError('Email is already registered', { taken: true });
+      } else {
+        context.result = { taken: false };
         return context;
       }
     }
     case data.phone_number: {
       const phone_number = data.phone_number;
-      let isPhone = await app.service('users')._find({query: {phone_number}}, params);
+      let isPhone = await app.service('users')._find({ query: { phone_number } }, params);
 
-      if(isPhone.total) {
-        throw new GeneralError('Phone number is already registered', {taken: true});
-      }else {
-        context.result = {taken: false};
+      if (isPhone.total) {
+        throw new GeneralError('Phone number is already registered', { taken: true });
+      } else {
+        context.result = { taken: false };
         return context;
       }
     }
